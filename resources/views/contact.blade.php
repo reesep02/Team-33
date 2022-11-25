494ab654c1d56ce2c20066609a8a5f1898b220f5
Qalud <khaled.qasim183@gmail.com>
2022-11-25 22:33:59 +0000

    d

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -102,7 +102,7 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ url('/') }}" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
