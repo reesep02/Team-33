09fb8c0241f340d6c4472db7e64f2e94d9780eec
Qalud <khaled.qasim183@gmail.com>
2022-11-25 23:40:55 +0000

    d

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -47,6 +47,7 @@
 
     <title>{{ config('app.name', 'Jewelz') }}</title>
 
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -54,9 +55,10 @@
 
     <!-- CSS -->
     {{-- <link rel="stylesheet" href="{{ asset('app.css') }}"> --}}
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     {{-- <script src="{{ asset('app.js') }}"></script> --}}
 </head>
 <body>
