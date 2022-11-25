b31d02a1a36bc9ba551df10af6ffd85c3883b398
Qalud <khaled.qasim183@gmail.com>
2022-11-25 22:41:48 +0000

    d

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -101,6 +101,18 @@
                                 </li>
                             @endif
                         @else
+                        <div style="color: black">
+                            <h1>Hello</h1>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                        </div>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
@@ -118,6 +130,7 @@
                                     </form>
                                 </div>
                             </li>
+
                         @endguest
                     </ul>
                 </div>
