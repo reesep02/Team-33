d2cc48cc9c7a6fbb1f0fa93b29e8a66f71cfafef
Qalud <khaled.qasim183@gmail.com>
2022-11-25 01:28:47 +0000

    made the home page extened the app page which also now contains a constant footer in all pages

--- resources/views/home-old.blade.php
+++ resources/views/home-old.blade.php
@@ -1,10 +1,9 @@
-@extends('layouts.app')
-
-
-@section('content')
-
+<!DOCTYPE html>
+<html>
 <header id="header">
-
+    <link rel="stylesheet" href="{{ asset('app.css') }}">
+    <script src="{{ asset('app.js') }}"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <img src="{{ asset('images/JWZ_Logo.png') }}" style="width:50px;height:50px;">
     <h1>JEWELZ</h1>
     <div class="user-details ">
@@ -95,8 +94,57 @@
 
 </main>
 
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+                <h6>ABOUT JEWELZ</h6>
+                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                    aute irure dolor in reprehenderit in voluptate velit esse.</p>
+            </div>
+            <br>
+            <div class="col-xs-6 col-md-3">
+                <h6>CATEGORIES</h6>
+                <ul class="footer-links">
+                    <li>
+                        <a href="{{ url('/') }}">NECKLACES</a>
+                    </li>
+                    <li><a href="{{ url('/') }}">RINGS</a></li>
+                    <li>
+                        <a href="{{ url('/') }}">BRACELETS</a>
+                    </li>
+                    <li><a href="{{ url('/') }}">EARRINGS</a></li>
+                </ul>
+            </div>
+            <br>
+            <div class="col-xs-6 col-md-3">
+                <h6>DISCOVER JEWELZ</h6>
+                <ul class="footer-links">
+                    <li><a href="{{ url('/') }}">ABOUT US</a></li>
+                    <li><a href="{{ url('/') }}">CONTACT US</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+                <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by
+                    <a href="{{ url('/') }}">Jewelz</a>.
+                </p>
+            </div>
 
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <ul class="social-icons">
+                    <li><a class="facebook" href="{{ url('/') }}"><i class="fa fa-facebook"></i></a></li>
+                    <li><a class="twitter" href="{{ url('/') }}"><i class="fa fa-twitter"></i></a></li>
+                    <li><a class="instagram" href="{{ url('/') }}"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
 
-
-@endsection
-
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ asset('app.css') }}">
+
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <script src="{{ asset('app.js') }}"></script>
 </head>
-<body>
+{{-- <body>
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow p-7 mb-4">
             <div class="container-fluid">
@@ -86,5 +90,62 @@
             @yield('content')
         </main>
     </div>
-</body>
+</body> --}}
+<div id="app">
+    <main>
+        @yield('content')
+    </main>
+ </div>
+ <footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+                <h6>ABOUT JEWELZ</h6>
+                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                    aute irure dolor in reprehenderit in voluptate velit esse.</p>
+            </div>
+            <br>
+            <div class="col-xs-6 col-md-3">
+                <h6>CATEGORIES</h6>
+                <ul class="footer-links">
+                    <li>
+                        <a href="{{ url('/') }}">NECKLACES</a>
+                    </li>
+                    <li><a href="{{ url('/') }}">RINGS</a></li>
+                    <li>
+                        <a href="{{ url('/') }}">BRACELETS</a>
+                    </li>
+                    <li><a href="{{ url('/') }}">EARRINGS</a></li>
+                </ul>
+            </div>
+            <br>
+            <div class="col-xs-6 col-md-3">
+                <h6>DISCOVER JEWELZ</h6>
+                <ul class="footer-links">
+                    <li><a href="{{ url('/') }}">ABOUT US</a></li>
+                    <li><a href="{{ url('/') }}">CONTACT US</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+                <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by
+                    <a href="{{ url('/') }}">Jewelz</a>.
+                </p>
+            </div>
+
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <ul class="social-icons">
+                    <li><a class="facebook" href="{{ url('/') }}"><i class="fa fa-facebook"></i></a></li>
+                    <li><a class="twitter" href="{{ url('/') }}"><i class="fa fa-twitter"></i></a></li>
+                    <li><a class="instagram" href="{{ url('/') }}"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
 </html>
