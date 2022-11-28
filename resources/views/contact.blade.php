b93b2fa7c90c58c433456428f5e417cd6849a182
MO MO <200099286@aston.ac.uk>
2022-11-28 20:36:50 +0000

    finsihing the about us section in html

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,5 +9,29 @@
   <link rel="stylesheet" type="text/css" href="about.css">
 </head>
 
+<body>
+
+<div class="wrapper">
+  <div class="atitle">
+    <h1> about Jewelz</h1>
+
+  </div>
+  <div class="about">
+    <div class="logo">
+      <img src="tile-wide.png">
+    </div>
+    <article>
+      <h3>At our store, you will find the best jewelry at very reasonable prices. Our huge range includes diamonds, gold and silver jewellery as well as special pieces made. All our products are handmade by skilled craftsmen in our UK factories using traditional techniques passed down through generations.
+      </h3>
+   <p>We work closely with our suppliers to ensure that only the best quality materials are used in the making of our jewellery so that you can be assured of its durability and beauty.</p>
+
+    </article>
+
+
+  </div>
+</div>
+
+</body>
+
 
 @endsection
