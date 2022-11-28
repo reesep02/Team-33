9e282271be670d5c6ec4c948dbdb22fc7ab4916d
Faizan Naseem <210057917@aston.ac.uk>
2022-11-28 16:59:12 +0000

    Shopping Cart page

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,9 +1,48 @@
 
 @extends('layouts.app')
 
+
 @section('content')
 
-<h1>this is the cart page</h1>
+<link rel="stylesheet" href="app.css">
+
+<section class="Shopping Cart">
+    <div class="container">
+            <div class="row justify-content-center text-center">
+                    <div class="col-md-8 col-lg-6">
+                            <div class="header" style="padding-top: 7em;padding-bottom: 2em; ">
+                                    <h1>Shopping Cart</h1>
+                            </div>
+                    </div>
+            </div>
+
+<section id="cart-container" class="container my-5">
+    <table width="100%">
+        <thead>
+            <tr>
+                <td><h3>Remove</h3></td>
+                <td><h3>Image</h3></td>
+                <td><h3>Product</h3></td>
+                <td><h3>Price</h3></td>
+                <td><h3>Quantity</h3></td>
+                <td><h3>Total</h3></td>
+            </tr>    
+        </thead>
+        <tbody>
+            <tr>
+                <td><button class="remove-product"> Remove </button></td>
+                <td><img src="" alt=""></img></td>
+                <td><h4>Product Name</h4></td>
+                <td>300£</td>
+                <td><input class="w-25 pl-1" value="1" type = "number"></td>
+
+            </tr>
+        </tbody>
+    
+    </table>
+</section>
+
+
 
 
 @endsection
