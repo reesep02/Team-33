1c62b6fbd581295c71f4a7ff10be39b400af7f7d
Qalud <khaled.qasim183@gmail.com>
2022-11-29 15:26:00 +0000

    typo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,9 +80,9 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('cart.index') }}"><img src="{{ asset('images/cart.png') }}">
-                        @if (Cart::instance('default')->count() > 0)
+                        {{-- @if (Cart::instance('default')->count() > 0)
                             <span>{{ Cart::instance('default')->count() }}</span>
-                        @endif
+                        @endif --}}
                     </a>
                 </li>
                 @endguest
