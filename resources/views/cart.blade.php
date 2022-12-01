377e14f382bac8adfc3711a19443c1f707099f9a
Qalud <khaled.qasim183@gmail.com>
2022-12-01 11:50:12 +0000

    Cart fully works now, navabr needs some css work, only thing missing is quantity update in cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -12,15 +12,107 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name', 'Jewelz') }}</title>
+    <link rel="stylesheet" href="{{ asset('app.css') }}">
 </head>
+<header class="app-navbar-header">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('images/JWZ_Logo.png') }}" alt="logo"  class="d-inline-block align-text-center">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+               <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/#rings') }}">RINGS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/#necklaces') }}">NECKLACES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/#bracelets') }}">BRACELETS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/#earrings') }}">EARRINGS</a>
+                </li>
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <img src="{{ asset('images/user.png') }}" >
+                        {{ Auth::user()->name }}
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a></li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                        <li><a class="dropdown-item" href="{{url('myProfile')}}">My Profile</a></li>
+                        {{-- <li><a class="dropdown-item" href="#">Another action</a></li> --}}
+                        {{-- follow this formate to add more sections to the nav item dropdown button --}}
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('cart.index') }}"><img src="{{ asset('images/cart.png') }}">
+                        @if (Cart::instance('default')->count() > 0)
+                            <span>{{ Cart::instance('default')->count() }}</span>
+                        @endif
+                    </a>
+                </li>
+                @endguest
+              </ul>
+            </div>
+        </div>
+    </nav>
+</header>
 <body>
     @guest
     <h1>Login to view cart<h1>
     @else
-    <div class="container-fluid">
+
+    <div style="margin-top: 10rem; margin-bottom: 2rem" class="container-fluid">  <!-- Delete this margin-top when navbar is fixed !-->
         <div class="row">
             <aside class="col-lg-9">
+                @if (session()->has('success_message'))
+                    <div class="alert alert-success">
+                        {{ session()->get('success_message') }}
+                    </div>
+                @endif
+
+                @if(count($errors) > 0)
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <h1>My Cart:</h1>
+                <div>
+
+
+                @if (Cart::count() > 0)
                 <div class="card">
+
+                    <h2>{{ Cart::count() }} item(s) in Shopping Cart</h2>
                     <div class="table-responsive">
                         <table class="table table-borderless table-shopping-cart">
                             <thead class="text-muted">
@@ -32,27 +124,40 @@
                                 </tr>
                             </thead>
                             <tbody>
+                                @foreach (Cart::content() as $item)
                                 <tr>
                                     <td>
                                         <figure class="itemside align-items-center">
                                             <div class="aside"><img src="https://i.imgur.com/1eq5kmC.png" class="img-sm"></div>
-                                            <figcaption class="info"> <a href="#" class="title text-dark" data-abc="true">Tshirt with round nect</a>
-                                                <p class="text-muted small">SIZE: L <br> Brand: MAXTRA</p>
+                                            <figcaption class="info"> <a href="{{ route('shop.show', $item->model->name) }}" class="title text-dark" data-abc="true">{{ $item->model->name }}</a>
+                                                {{-- <p class="text-muted small">SIZE: L </p> --}}
                                             </figcaption>
                                         </figure>
                                     </td>
-                                    @foreach (Cart::content() as $item)
+
                                     <td> <select class="quantity" data-id="{{ $item->rowId }}" data-productQuantity="{{ $item->model->quantity }}">
                                             @for ($i = 1; $i < 5 + 1 ; $i++)
                                                 <option {{ $item->qty == $i ? 'selected' : '' }}>{{ $i }}</option>
                                             @endfor
                                         </select></td>
-                                    @endforeach
+
                                     <td>
-                                        <div class="price-wrap"> <var class="price">$10.00</var> <small class="text-muted"> $9.20 each </small> </div>
+                                        <div class="price-wrap"> <var class="price">{{ $item->model->presentPrice() }}</var></div>
                                     </td>
-                                    <td class="text-right d-none d-md-block"> <a data-original-title="Save to Wishlist" title="" href="" class="btn btn-light" data-toggle="tooltip" data-abc="true"> <i class="fa fa-heart"></i></a> <a href="" class="btn btn-light" data-abc="true"> Remove</a> </td>
+                                    <td class="text-right d-none d-md-block">
+                                        <form action="{{ route('cart.switchToSaveForLater', $item->rowId) }}" method="POST">
+                                            {{ csrf_field() }}
+
+                                            <button type="submit" data-original-title="Save to Wishlist" data-abc="true" class="cart-options btn btn-light"><i class="fa fa-heart"></i></button>
+                                        </form>
+                                        <form action="{{ route('cart.destroy', $item->rowId) }}" method="POST">
+                                            {{ csrf_field() }}
+                                            {{ method_field('DELETE') }}
+                                            <button type="submit" data-abc="true" style="margin-top: 1rem" class="cart-options btn btn-light">Remove</button>
+                                        </form>
+                                    </td>
                                 </tr>
+                                @endforeach
                                 {{-- <tr>
                                     <td>
                                         <figure class="itemside align-items-center">
@@ -76,10 +181,18 @@
                             </tbody>
                         </table>
                     </div>
+                @else
+                <div class="card">
+                    <h2>No Items In Shopping cart</h2>
+                </div>
+                @endif
                 </div>
             </aside>
+
+
+
             <aside class="col-lg-3">
-                <div class="card mb-3">
+                {{-- <div class="card mb-3">
                     <div class="card-body">
                         <form>
                             <div class="form-group"> <label>Have coupon?</label>
@@ -87,59 +200,70 @@
                             </div>
                         </form>
                     </div>
-                </div>
+                </div> --}}
                 <div class="card">
                     <div class="card-body">
                         <dl class="dlist-align">
-                            <dt>Total price:</dt>
-                            <dd class="text-right ml-3">$69.97</dd>
+                            <dt>Price:</dt>
+                            <dd class="text-right ml-3"> {{ presentPrice(Cart::subtotal()) }}</dd>
                         </dl>
                         <dl class="dlist-align">
-                            <dt>Discount:</dt>
-                            <dd class="text-right text-danger ml-3">- $10.00</dd>
+                            <dt>20% VAT:</dt>
+                            <dd class="text-right ml-3">{{ presentPrice(Cart::tax()) }}</dd>
                         </dl>
                         <dl class="dlist-align">
-                            <dt>Total:</dt>
-                            <dd class="text-right text-dark b ml-3"><strong>$59.97</strong></dd>
+                            <dt>Price With VAT:</dt>
+                            <dd class="text-right text-dark b ml-3"><strong>{{ presentPrice(Cart::total()) }}</strong></dd>
                         </dl>
-                        <hr> <a href="#" class="btn btn-out btn-primary btn-square btn-main" data-abc="true"> Make Purchase </a> <a href="#" class="btn btn-out btn-success btn-square btn-main mt-2" data-abc="true">Continue Shopping</a>
+                        <hr> <a href="{{ route('checkout.index') }}" class="btn btn-out btn-primary btn-square btn-main" data-abc="true"> Make Purchase </a> <a href="{{ url('/') }}" class="btn btn-out btn-success btn-square btn-main mt-2" data-abc="true">Continue Shopping</a>
                     </div>
                 </div>
             </aside>
             <aside class="col-lg-9">
+                <h1 style="margin-top: 1rem">My WhishList:</h1>
+                @if (Cart::instance('saveForLater')->count() > 0)
                 <div class="card">
+                    <h1>{{ Cart::instance('saveForLater')->count() }} item(s) In WishList</h1>
                     <div class="table-responsive">
                         <table class="table table-borderless table-shopping-cart">
                             <thead class="text-muted">
                                 <tr class="small text-uppercase">
                                     <th scope="col">Product</th>
-                                    <th scope="col" width="120">Quantity</th>
-                                    <th scope="col" width="120">Price</th>
                                     <th scope="col" class="text-right d-none d-md-block" width="200"></th>
                                 </tr>
                             </thead>
                             <tbody>
+                                @foreach (Cart::instance('saveForLater')->content() as $item)
                                 <tr>
                                     <td>
                                         <figure class="itemside align-items-center">
                                             <div class="aside"><img src="https://i.imgur.com/1eq5kmC.png" class="img-sm"></div>
-                                            <figcaption class="info"> <a href="#" class="title text-dark" data-abc="true">Tshirt with round nect</a>
-                                                <p class="text-muted small">SIZE: L <br> Brand: MAXTRA</p>
+                                            <figcaption class="info"> <a href="#" class="title text-dark" data-abc="true">{{$item->model->name}}</a>
+                                                {{-- <p class="text-muted small">SIZE: L</p> --}}
                                             </figcaption>
                                         </figure>
                                     </td>
-                                    @foreach (Cart::content() as $item)
-                                    <td> <select class="quantity" data-id="{{ $item->rowId }}" data-productQuantity="{{ $item->model->quantity }}">
-                                            @for ($i = 1; $i < 5 + 1 ; $i++)
-                                                <option {{ $item->qty == $i ? 'selected' : '' }}>{{ $i }}</option>
-                                            @endfor
-                                        </select></td>
-                                    @endforeach
+
+
+
                                     <td>
-                                        <div class="price-wrap"> <var class="price">$10.00</var> <small class="text-muted"> $9.20 each </small> </div>
+                                        <div class="price-wrap"> <var class="price">{{$item->model->presentPrice()}}</var></div>
                                     </td>
-                                    <td class="text-right d-none d-md-block"> <a data-original-title="Save to Wishlist" title="" href="" class="btn btn-light" data-toggle="tooltip" data-abc="true"> <i class="fa fa-heart"></i></a> <a href="" class="btn btn-light" data-abc="true"> Remove</a> </td>
+                                    <td class="text-right d-none d-md-block">
+                                        <form action="{{ route('saveForLater.destroy', $item->rowId) }}" method="POST">
+                                            {{ csrf_field() }}
+                                            {{ method_field('DELETE') }}
+
+                                            <button type="submit" data-abc="true" class="btn btn-light">Remove</button>
+                                        </form>
+                                        <form action="{{ route('saveForLater.switchToCart', $item->rowId) }}" method="POST">
+                                            {{ csrf_field() }}
+
+                                            <button type="submit" data-abc="true" style="margin-top: 1rem" class="btn btn-light">Move to Cart</button>
+                                        </form>
+                                    </td>
                                 </tr>
+                                @endforeach
                                 {{-- <tr>
                                     <td>
                                         <figure class="itemside align-items-center">
@@ -164,6 +288,11 @@
                         </table>
                     </div>
                 </div>
+                @else
+                <div class="card">
+                    <h2>No Items In WhishList</h2>
+                </div>
+                @endif
             </aside>
         </div>
     </div>
@@ -201,4 +330,58 @@
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
+
+
+<footer class="site-footer sticky-bottom footer mt-auto py-3 bg-dark app-footer-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+                <h6>ABOUT JEWELZ</h6>
+                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                    aute irure dolor in reprehenderit in voluptate velit esse.</p>
+            </div>
+            <br>
+            <div class="col-xs-6 col-md-3">
+                <h6>CATEGORIES</h6>
+                <ul class="footer-links">
+                    <li>
+                        <a href="{{ url('/#necklaces') }}">NECKLACES</a>
+                    </li>
+                    <li><a href="{{ url('/#rings') }}">RINGS</a></li>
+                    <li>
+                        <a href="{{ url('/#bracelets') }}">BRACELETS</a>
+                    </li>
+                    <li><a href="{{ url('/#earrings') }}">EARRINGS</a></li>
+                </ul>
+            </div>
+            <br>
+            <div class="col-xs-6 col-md-3">
+                <h6>DISCOVER JEWELZ</h6>
+                <ul class="footer-links">
+                    <li><a href="{{ url('/about') }}">ABOUT US</a></li>
+                    <li><a href="{{ url('/contact') }}">CONTACT US</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+                <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by
+                    <a href="{{ url('/') }}">Jewelz</a>.
+                </p>
+            </div>
+
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <ul class="social-icons">
+                    <li><a class="facebook" href="{{ url('/') }}"><i class="fa fa-facebook"></i></a></li>
+                    <li><a class="twitter" href="{{ url('/') }}"><i class="fa fa-twitter"></i></a></li>
+                    <li><a class="instagram" href="{{ url('/') }}"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
 
