9888b16ebc41c8966c761ea42c0b0b927a5675c7
MO MO <200099286@aston.ac.uk>
2022-12-01 14:34:35 +0000

    linking about the us page and cotact us to the right sections

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -114,7 +114,7 @@
 
   <div class="atitle">
     <p></p>
-    <h1> Get in touch</h1>
+    <h1 id="contact"> Get in touch</h1>
 
   </div>
   <div class="wrap">
@@ -137,7 +137,7 @@
   </div>
   <div class="container1">
         <form>
-            <h1 id="contact">Contact Us Below</h1>
+            <h1>Contact Us Below</h1>
             <input type="text" id="firstName" placeholder="First Name" required>
             <input type="text" id="lastName" placeholder="Last Name" required>
             <input type="email" id="email" placeholder="Email" required>
