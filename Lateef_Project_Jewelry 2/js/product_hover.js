d36f551d6fbdab846d10a4c134e876d69df45a06
lateefolasimbo <lateefolasimbo16@gmail.com>
2022-12-01 17:20:23 +0000

    A new design for multiview product page

---
+++ Lateef_Project_Jewelry 2/js/product_hover.js
@@ -0,0 +1,8 @@
+function mouseHover(){
+    var product = document.getElementsByClassName("product-description")
+    for (let index = 0; index < product.length; index++) {
+        product[i].style.display = "block"
+        
+    }
+   
+}
