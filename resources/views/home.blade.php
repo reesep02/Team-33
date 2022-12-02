5b26e9fd7418993ca36747c160e749ce01969681
Bernard Mithi <210093454@aston.ac.uk>
2022-12-02 16:47:44 +0000

    Changes to Banner for responsiveness and Changing place holder text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,20 +7,22 @@
 
 <main>
 
-    <div class="section one">
-        <p class="t2">EXPRESS YOURSELF</p>
+    <div class="section one ">
+        <p class=" t2">EXPRESS YOURSELF</p>
         </p>
-        <p class="t2">THE FINEST SUSTAINABLE JEWELLERY</p>
-        <p class="t3">CRAFTED BY OUR VERY OWN JEWELERS</p>
+        <p class="t2">THE FINEST SUSTAINABLE UNISEX JEWELLERY</p>
+        <p class="t5">CRAFTED BY OUR VERY OWN JEWELERS</p>
+
     </div>
 
     <div class="section three" id="rings">
         <p class="t1">RINGS</p>
-        <p class="t2">SOMETHING CATCHY</p>
-        <p class="t3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute
-            irure dolor in reprehenderit in voluptate velit esse.</p>
+        <p class="t2">ADORN YOUR STYLE</p>
+        <p class="t3">Rings effortlessly add style to your favourite ensembles. Create a signature look with classic
+            bands for everyday, and intricate designs, perfect for stacking.Find style inspiration, no matter the
+            occasion. Each item from Jewelz has been handcrafted to the highest standards, so you can rely on
+            impeccable quality with every purchase. Browse our collection and find a ring to suit the recipient's taste
+            and style.</p>
         <p class="t4">
             <a href="{{ url('/shop-ring') }}">BROWSE</a>
         </p>
@@ -31,11 +33,9 @@
 
     <div class="section four" id="necklaces">
         <p class="t1">NECKLACES</p>
-        <p class="t2">SOMETHING CATCHY</p>
-        <p class="t3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute
-            irure dolor.</p>
+        <p class="t2">HANDCRAFTED SILVER AND GOLD</p>
+        <p class="t3">Necklaces are the perfect finishing touch for any outfit. With dainty pieces and statement designs
+            to enhance your look, you'll find exactly what you need to showcase your personal style.</p>
         <p class="t4"><a href="{{ url('/shop-necklace') }}">BROWSE</a></p>
     </div>
     <div id='section4'>
@@ -44,10 +44,10 @@
 
     <div class="section five" id="bracelets">
         <p class="t1">BRACELETS</p>
-        <p class="t2">SOMETHING CATCHY</p>
-        <p class="t3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo.</p>
+        <p class="t2">NEW LAUNCHED COLLECTION</p>
+        <p class="t3">Bracelets are the classic jewellery that adds the finishing touch to a Jewellery set. From gold
+            and silver bracelets, to minimalist bangles for every occasion. We have pieces for all your styling needs.
+        </p>
         <p class="t4"><a href="{{ url('/shop-bracelet') }}">BROWSE</a></p>
     </div>
     <div id='section5'>
@@ -56,11 +56,9 @@
 
     <div class="section six" id="earrings">
         <p class="t1">EARRINGS</p>
-        <p class="t2">SOMETHING CATCHY</p>
-        <p class="t3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute
-            irure dolor in reprehenderit in voluptate velit esse.</p>
+        <p class="t2">STYLE ESSENTIALS</p>
+        <p class="t3">Look and feel amazing in a pair of our sensationally sparkling diamond earrings. Handcrafted with
+            only the finest conflict-free <br> diamonds, they'll add a touch of luxury to any occasion.</p>
         <p class="t4"><a href="{{ url('/shop-earring') }}">BROWSE</a></p>
     </div>
     <div id='section6'>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,78 +16,82 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('app.css') }}">
     <!-- Scripts -->
     <script src="{{asset('app.js')}}"></script>
     {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </head>
 <header class="app-navbar-header">
     <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="{{ asset('images/JWZ_Logo.png') }}" alt="logo"  class="d-inline-block align-text-center">
+                <img src="{{ asset('images/JWZ_Logo.png') }}" alt="logo" class="d-inline-block align-text-center">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
-               <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/shop-ring') }}">RINGS</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/shop-necklace') }}">NECKLACES</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/shop-bracelet') }}">BRACELETS</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/shop-earring') }}">EARRINGS</a>
-                </li>
-                @guest
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/shop-ring') }}">RINGS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/shop-necklace') }}">NECKLACES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/shop-bracelet') }}">BRACELETS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/shop-earring') }}">EARRINGS</a>
+                    </li>
+                    @guest
                     @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
                     @endif
 
                     @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
                     @endif
-                @else
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        <img src="{{ asset('images/user.png') }}" >
-                        {{ Auth::user()->name }}
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
+                    @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false" v-pre>
+                            <img src="{{ asset('images/user.png') }}">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                             document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a></li>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                        <li><a class="dropdown-item" href="{{url('myProfile')}}">My Profile</a></li>
-                        {{-- <li><a class="dropdown-item" href="#">Another action</a></li> --}}
-                        {{-- follow this formate to add more sections to the nav item dropdown button --}}
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('cart.index') }}"><img src="{{ asset('images/cart.png') }}">
-                        @if (Cart::instance('default')->count() > 0)
+                                    {{ __('Logout') }}
+                                </a></li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                            <li><a class="dropdown-item" href="{{url('myProfile')}}">My Profile</a></li>
+                            {{-- <li><a class="dropdown-item" href="#">Another action</a></li> --}}
+                            {{-- follow this formate to add more sections to the nav item dropdown button --}}
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('cart.index') }}"><img src="{{ asset('images/cart.png') }}">
+                            @if (Cart::instance('default')->count() > 0)
                             <span style="color: #f7c02b; size:20px">{{ Cart::instance('default')->count() }}</span>
-                        @endif
-                    </a>
-                </li>
-                @endguest
-              </ul>
+                            @endif
+                        </a>
+                    </li>
+                    @endguest
+                </ul>
             </div>
         </div>
     </nav>
@@ -97,16 +102,22 @@
     <main>
         @yield('content')
     </main>
- </div>
+</div>
 
- <body class="d-flex flex-column min-vh-100">
+<body class="d-flex flex-column min-vh-100">
     <footer class="site-footer sticky-bottom footer mt-auto py-3 bg-dark app-footer-footer">
         <div class="container">
             <div class="row">
                 <div class="col-sm-12 col-md-6">
                     <h6>ABOUT JEWELZ</h6>
-                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                        aute irure dolor in reprehenderit in voluptate velit esse.</p>
+                    <p class="text-justify">
+                        At our store, you will find the best jewelry at very reasonable prices. Our huge range includes
+                        diamonds, gold and silver jewellery as well as special pieces made. All our products are
+                        handmade by skilled craftsmen in our UK factories using traditional techniques passed down
+                        through generations. We combine modern design with sustainability. We use recycled silver and
+                        ethical diamonds so that our helix earrings, dainty bracelets, and curb chains look great,
+                        whilst keeping the planet in mind.
+                    </p>
                 </div>
                 <br>
                 <div class="col-xs-6 col-md-3">
@@ -152,7 +163,8 @@
             </div>
         </div>
     </footer>
-<body class="d-flex flex-column min-vh-100">
+
+    <body class="d-flex flex-column min-vh-100">
 
 
 </html>
